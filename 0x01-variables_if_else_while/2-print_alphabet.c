d87bd8549007ca5a2eef9e9d7fd20dead56e551b
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-12 18:06:47 -0700

    alphanumerals

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int v = 97;
+
 	while (v <= 122)
 	{
 		putchar(v);
