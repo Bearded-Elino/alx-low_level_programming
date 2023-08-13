7d8a424265f6f599e688a2e55be054a26799a980
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-12 20:39:44 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int s;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (s = 97; s <= 102; n++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
