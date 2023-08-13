ff38b0086110fea9b01cb66c34cfc8004f50625c
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-12 17:59:02 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return Always 0 (Success)
+ */
+int main (void)
+{
+	int v = 97;
+	while (v <= 122)
+	{
+		putchar(v);
+		v++;
+	}
+	putchar(v);
+	return (0);
+}
