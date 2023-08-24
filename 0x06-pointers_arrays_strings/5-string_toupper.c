e73f8e22da6d2a507f20a45c9ee12f7bce594607
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-24 13:53:25 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper(char *n) - change all lowercase to uppercase
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	int 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
