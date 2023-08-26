781b184792bb6ae4e110fdafc414227ee789bee6
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-25 17:21:49 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
- * @str: The string to be capitalized
+ * cap_string - capitalizes all words of a string.
+ * @str: The string to be capitalized.
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -12,36 +12,38 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] == ' ' !!
 
-			str[index -1] == '\t' !!
+			if (str[index - 1] == ' ' ||
 
-			str[index -1] == '\n' !!
+			str[index -1] == '\t' ||
 
-			str[index -1] == ',' !!
+			str[index -1] == '\n' ||
 
-			str[index -1] == ',' !!
+			str[index -1] == ',' ||
 
-			str[index -1] == ',' !!
+			str[index -1] == ';' ||
 
-			str[index -1] == ')' !!
+			str[index -1] == '.' ||
 
-			str[index -1] == '(' !!
+			str[index -1] == '!' ||
 
-			str[index -1] == '"' !!
+			str[index -1] == '?' ||
 
-			str[index -1] == '"' !!
+			str[index -1] == '"' ||
 
-			str[index -1] == '?' !!
+			str[index -1] == '(' ||
 
-			str[index -1] == '{' !!
+			str[index -1] == ')' ||
 
-			str[index -1] == '}' !!
+			str[index -1] == '{' ||
+
+			str[index -1] == '}' ||
 
 			index == 0)
 
 			str[index] -= 32;
-		index++;
+
+			index++;
 	}
 	return (str);
 }
