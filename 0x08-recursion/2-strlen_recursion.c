733d56c18b71b1620f75cd03dba639aad4199d22
Bearded-Elino <elijahonuh2@gmail.com>
2023-08-31 05:34:58 -0700

    task_2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+void _putchar(char c);
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: pointer to string.
+ *
+ * Return: length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
